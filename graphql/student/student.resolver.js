a3ada634130a9eb19ccd9473b50ad6388f55229a
tholib.hanafi <tholib.hanafi@zettacamp.pro>
2025-06-04 10:44:20 +0700

    [Module_1][Implement CRUD Resolvers for Student Model][ZEBTO-12]Develop the GraphQL Resolver(Mutation-Student)

--- graphql/student/student.resolver.js
+++ graphql/student/student.resolver.js
@@ -4,24 +4,49 @@ import Student from './student.model.js';
 // *************** RESOLVER ***************
 const StudentResolver = {
 
-  // *************** QUERY ***************
-  Query: {
-    // Get all students (excluding soft-deleted)
-    GetAllStudents: async () => {
-      return await Student.find({deleted_at: null});
-    },
+    // *************** QUERY ***************
+    Query: {
+        // Get all students (excluding soft-deleted)
+        GetAllStudents: async () => {
+            return await Student.find({ deleted_at: null });
+        },
 
-    // Get a specific student by ID (if not deleted)
-    GetOneStudent: async (_, { id }) => {
-      return await Student.findOne({ _id: id, deleted_at: null });
+        // Get a specific student by ID (if not deleted)
+        GetOneStudent: async (_, { id }) => {
+            return await Student.findOne({ _id: id, deleted_at: null });
+        },
     },
-  },
-  Student: {
-    // Resolve school relation
-    school: async (student, _, { loaders }) => {
-      return await loaders.school.load(student.schoolId.toString());
+    Mutation: {
+        // create new Student
+        CreateStudent: async (_, { input }) => {
+            const student = new Student(input)
+            return await student.save()
+        },
+        // Update existing student by ID
+        UpdateUser: async (_, { id, input }) => {
+            return await User.findOneAndUpdate(
+                { _id: id },
+                { $set: input },
+                { new: true }
+            );
+        },
+
+        // Soft delete a student by ID
+        DeleteUser: async (_, { id }) => {
+            const deletedUser = await User.findOneAndUpdate(
+                { _id: id },
+                { $set: { deleted_at: new Date() } },
+                { new: true }
+            );
+            return deletedUser;
+        },
+    },
+    Student: {
+        // Resolve school relation
+        school: async (student, _, { loaders }) => {
+            return await loaders.school.load(student.schoolId.toString());
+        }
     }
-  }
 };
 
 // *************** EXPORT RESOLVER ***************
