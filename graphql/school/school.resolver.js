4b6c3213e2dca3413646547161ee08de93a39318
tholib.hanafi <tholib.hanafi@zettacamp.pro>
2025-06-05 11:15:24 +0700

    [Module_1][Implement DataLoader for School-Student Relationship][ZEBTO-10] Add School Validation

--- graphql/school/school.resolver.js
+++ graphql/school/school.resolver.js
@@ -1,49 +1,86 @@
-// *************** IMPORT MODEL ***************
+// *************** IMPORT MODULE ***************
 const School = require("./school.model.js");
+
+// *************** IMPORT VALIDATOR ***************
+const {
+  validateCreateSchoolInput,
+  validateUpdateSchoolInput,
+} = require("./school.validator.js");
+
+// *************** IMPORT ERROR HANDLER ***************
+const { handleCaughtError } = require("../../utils/error.helper.js");
 
 // *************** QUERY ***************
 
 // *************** Get all schools (excluding soft-deleted)
 const GetAllSchools = async () => {
-  return await School.find({ deleted_at: null });
+  try {
+    return await School.find({ deleted_at: null });
+  } catch (error) {
+    throw handleCaughtError(error, "Failed to fetch schools.");
+  }
 };
 
 // *************** Get a specific school by ID (if not deleted)
 const GetOneSchool = async (_, { id }) => {
-  return await School.findOne({ _id: id, deleted_at: null });
+  try {
+    return await School.findOne({ _id: id, deleted_at: null });
+  } catch (error) {
+    throw handleCaughtError(error, "Failed to fetch school.");
+  }
 };
 
 // *************** MUTATION ***************
 
 // *************** Create new School
 const CreateSchool = async (_, { input }) => {
-  const school = new School(input);
-  return await school.save();
+  try {
+    // *************** Validate input
+    validateCreateSchoolInput(input);
+
+    const school = new School(input);
+    return await school.save();
+  } catch (error) {
+    throw handleCaughtError(error, "Failed to create school.");
+  }
 };
 
 // *************** Update existing school by ID
 const UpdateSchool = async (_, { id, input }) => {
-  return await School.findOneAndUpdate(
-    { _id: id },
-    { $set: input },
-    { new: true }
-  );
+  try {
+    // *************** Validate input
+    validateUpdateSchoolInput(input);
+
+    return await School.findOneAndUpdate(
+      { _id: id },
+      { $set: input },
+      { new: true }
+    );
+  } catch (error) {
+    throw handleCaughtError(error, "Failed to update school.");
+  }
 };
 
 // *************** Soft delete a school by ID
 const DeleteSchool = async (_, { id }) => {
-  return await School.findOneAndUpdate(
-    { _id: id },
-    { $set: { deleted_at: new Date() } },
-    { new: true }
-  );
+  try {
+    return await School.findOneAndUpdate(
+      { _id: id },
+      { $set: { deleted_at: new Date() } },
+      { new: true }
+    );
+  } catch (error) {
+    throw handleCaughtError(error, "Failed to delete school.");
+  }
 };
 
 // *************** Get all students in this school
 const students = (school, _, context) => {
+  // *************** Ensure DataLoader is initialized
   if (!context?.loaders?.student) {
     throw new Error("Student loader not initialized");
   }
+
   return context.loaders.student.load(school._id);
 };
 
---
+++ graphql/school/school.validator.js
@@ -0,0 +1,56 @@
+// *************** IMPORT HELPER FUNCTION ***************
+const { createAppError } = require("../../utils/error.helper.js");
+
+/**
+ * Validate input payload for creating a school.
+ *
+ * @param {Object} input - School input object.
+ * @param {string} input.name - Required school name.
+ * @param {string} [input.address] - Optional school address.
+ */
+function validateCreateSchoolInput(input) {
+  const { name, address } = input;
+
+  if (!name || typeof name !== "string") {
+    throw createAppError(
+      "Name is required and must be a string.",
+      "VALIDATION_ERROR",
+      { field: "name" }
+    );
+  }
+
+  if (address && typeof address !== "string") {
+    throw createAppError("Address must be a string.", "VALIDATION_ERROR", {
+      field: "address",
+    });
+  }
+}
+
+/**
+ * Validate input payload for updating a school.
+ *
+ * @param {Object} input - School update input object.
+ * @param {string} [input.name] - Optional new name.
+ * @param {string} [input.address] - Optional new address.
+ */
+function validateUpdateSchoolInput(input) {
+  const { name, address } = input;
+
+  if (name && typeof name !== "string") {
+    throw createAppError("Name must be a string.", "VALIDATION_ERROR", {
+      field: "name",
+    });
+  }
+
+  if (address && typeof address !== "string") {
+    throw createAppError("Address must be a string.", "VALIDATION_ERROR", {
+      field: "address",
+    });
+  }
+}
+
+// *************** EXPORT MODULE ***************
+module.exports = {
+  validateCreateSchoolInput,
+  validateUpdateSchoolInput,
+};
