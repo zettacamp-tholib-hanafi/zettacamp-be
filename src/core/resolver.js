512d3b49569e93eb619495bec05a56e14822671c
tholib.hanafi <tholib.hanafi@zettacamp.pro>
2025-06-13 10:52:37 +0700

    [Module_2][Development 2: Subject][BE_010] Create Type Definition

--- src/core/resolver.js
+++ src/core/resolver.js
@@ -3,8 +3,9 @@
 const userResolvers = require("../modules/user/user.resolver");
 const studentResolvers = require("../modules/student/student.resolver");
 const schoolResolvers = require("../modules/school/school.resolver");
+const blockResolver = require("../modules/block/block.resolver");
 
-const resolvers = [userResolvers, studentResolvers, schoolResolvers];
+const resolvers = [userResolvers, studentResolvers, schoolResolvers, blockResolver];
 
 // *************** EXPORT MODUL ***************
 
--- src/core/typedef.js
+++ src/core/typedef.js
@@ -3,8 +3,16 @@
 const userTypeDefs = require("../modules/user/user.typedef");
 const studentTypeDefs = require("../modules/student/student.typedef");
 const schoolTypeDefs = require("../modules/school/school.typedef");
+const blockTypeDefs = require("../modules/block/block.typedef");
+const subjectTypedef = require("../modules/subject/subject.typedef");
 
-const typeDefs = [userTypeDefs, studentTypeDefs, schoolTypeDefs];
+const typeDefs = [
+  userTypeDefs,
+  studentTypeDefs,
+  schoolTypeDefs,
+  blockTypeDefs,
+  subjectTypedef,
+];
 
 // *************** EXPORT MODUL ***************
 
