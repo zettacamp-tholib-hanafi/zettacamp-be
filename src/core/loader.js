429e522f7ec36f33267db5bb291d28fa3dfdc578
tholib.hanafi <tholib.hanafi@zettacamp.pro>
2025-06-13 10:52:14 +0700

    [Module_2][Development 1: Block][BE_008] Update Loader

--- src/core/loader.js
+++ src/core/loader.js
@@ -1,11 +1,13 @@
 // *************** IMPORT MODUL ***************
 
+const { BlockLoader } = require("../modules/block/block.loader");
 const { SchoolLoader } = require("../modules/school/school.loader");
 const { StudentLoader } = require("../modules/student/student.loader");
 
 const Loaders = {
   student: StudentLoader(),
   school: SchoolLoader(),
+  block: BlockLoader(),
 };
 
 // *************** EXPORT MODUL ***************
