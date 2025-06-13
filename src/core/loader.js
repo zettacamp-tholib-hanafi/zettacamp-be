24b7eaea7fbdf0279b9de104860dbbcdca759f43
tholib.hanafi <tholib.hanafi@zettacamp.pro>
2025-06-13 13:22:23 +0700

    [Module_2][Development 2: Subject][BE_016] Update Loader

--- src/core/loader.js
+++ src/core/loader.js
@@ -3,11 +3,13 @@
 const { BlockLoader } = require("../modules/block/block.loader");
 const { SchoolLoader } = require("../modules/school/school.loader");
 const { StudentLoader } = require("../modules/student/student.loader");
+const { SubjectLoader } = require("../modules/subject/subject.loader");
 
 const Loaders = {
   student: StudentLoader(),
   school: SchoolLoader(),
   block: BlockLoader(),
+  subject: SubjectLoader(),
 };
 
 // *************** EXPORT MODUL ***************
