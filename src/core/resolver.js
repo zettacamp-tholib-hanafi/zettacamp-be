b5e683f7664fbfaf553a260680f3d77fbc9bb168
tholib.hanafi <tholib.hanafi@zettacamp.pro>
2025-06-13 13:23:15 +0700

    [Module_2][Development 2: Subject][BE_015] Update Resolver: DeleteSubject Muatation

--- src/core/resolver.js
+++ src/core/resolver.js
@@ -4,8 +4,9 @@ const userResolvers = require("../modules/user/user.resolver");
 const studentResolvers = require("../modules/student/student.resolver");
 const schoolResolvers = require("../modules/school/school.resolver");
 const blockResolver = require("../modules/block/block.resolver");
+const subjectResolver = require("../modules/subject/subject.resolver");
 
-const resolvers = [userResolvers, studentResolvers, schoolResolvers, blockResolver];
+const resolvers = [userResolvers, studentResolvers, schoolResolvers, blockResolver, subjectResolver];
 
 // *************** EXPORT MODUL ***************
 
