b9b4e57998348be16ce6fcf1d18d343ae2637c2d
zettacamp-tholib-hanafi <tholib.hanafi@zettacamp.pro>
2025-06-17 10:44:04 +0700

    [Module_2][Development 5: Task][BE_034]Create TypeDef

---
+++ src/modules/task/task.typedef.js
@@ -0,0 +1,73 @@
+// *************** IMPORT LIBRARY ***************
+
+const gql = require("graphql-tag");
+
+// *************** EXPORT MODULE ***************
+
+module.exports = gql`
+  scalar Date
+
+  enum TaskType {
+    ASSIGN_CORRECTOR
+    ENTER_MARKS
+    VALIDATE_MARKS
+  }
+
+  enum TaskStatus {
+    PENDING
+    PROGRESS
+    COMPLETED
+    DELETED
+  }
+
+  type Task {
+    id: ID!
+    test_id: ID!
+    user_id: ID!
+    type: TaskType!
+    status: TaskStatus!
+    due_date: Date
+    created_at: Date
+    created_by: String
+    updated_at: Date
+    updated_by: String
+    deleted_at: Date
+    deleted_by: String
+  }
+
+  input CreateTaskInput {
+    test_id: ID!
+    user_id: ID!
+    type: TaskType!
+    status: TaskStatus!
+    due_date: Date
+    created_by: String
+  }
+
+  input UpdateTaskInput {
+    test_id: ID!
+    user_id: ID!
+    type: TaskType!
+    status: TaskStatus!
+    due_date: Date
+    updated_by: String
+  }
+
+  input TaskFilter {
+    test_id: ID
+    user_id: ID
+    type: TaskType
+    status: TaskStatus
+  }
+
+  type Query {
+    GetAllTasks(filter: TaskFilter): [Task!]!
+    GetOneTask(id: ID!, filter: TaskFilter): Task
+  }
+
+  type Mutation {
+    CreateTask(input: CreateTaskInput!): Task!
+    UpdateTask(id: ID!, input: UpdateTaskInput!): Task!
+    DeleteTask(id: ID!, deleted_by: String): Task!
+  }
+`;
