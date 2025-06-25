288744f978895d32fc408faa8598f7fb632c3ace
zettacamp-tholib-hanafi <tholib.hanafi@zettacamp.pro>
2025-06-25 10:48:35 +0700

    [Module_3][Phase 3: Transcript Calculation][BE_03_036] Transcript Calculation : Process each document

--- src/modules/calculationResult/calculation_result.worker.js
+++ src/modules/calculationResult/calculation_result.worker.js
@@ -14,7 +14,8 @@ const { CreateAppError } = require("../../core/error");
 
 // *************** IMPORT MODULE **************
 const StudentTestResult = require("../studentTestResult/student_test_result.model");
-const ConnectDB = require("../../core/db");
+const { ConnectDB, DisconnectDB } = require("../../core/db");
+const { Loaders } = require("../../core/loader");
 
 /**
  * RunTranscriptWorker
@@ -63,13 +64,8 @@ if (!isMainThread) {
       const { student_id } = workerData;
       const studentId = await ValidateMongoId(student_id);
 
-      const studentTestResult = await fetchStudentTestResult(studentId);
-      if (!studentTestResult) {
-        parentPort.postMessage({
-          success: false,
-          error: error.message || "Unknown Error in Transcript Worker",
-        });
-      }
+      await runTranscriptCore(studentId);
+
       parentPort.postMessage({
         success: true,
         student_id,
@@ -80,8 +76,48 @@ if (!isMainThread) {
         success: false,
         error: error.message || "Unknown Error in Transcript Worker",
       });
+    } finally {
+      DisconnectDB();
     }
   })();
+}
+
+/**
+ * Execute transcript process logic.
+ *
+ * @param {ObjectId} student_id
+ * @param {Object} loaders - Contains test, subject, and block DataLoaders
+ * @returns {Promise<Object>} Transcript result
+ */
+
+async function runTranscriptCore(student_id) {
+  const studentTestResults = await fetchStudentTestResult(student_id);
+  if (!studentTestResults) {
+    parentPort.postMessage({
+      success: false,
+      error: "Unknown Error in Transcript Worker",
+    });
+  }
+  console.log("WORKER: studentTestResults: ", studentTestResults);
+
+  const testIds = studentTestResults.map((result) => String(result.test_id));
+  const tests = await loadTestsByIds(testIds);
+  console.log("WORKER: testIds: ", tests);
+
+  const subjectIds = tests.map((test) => String(test.subject_id));
+  const subjects = await loadSubjectsByIds(subjectIds);
+  console.log("WORKER: subjectIds: ", subjects);
+
+  const blockIds = subjects.map((subject) => String(subject.block_id));
+  const blocks = await loadBlocksByIds(blockIds);
+  console.log("WORKER: blockIds: ", blocks);
+
+  return {
+    studentTestResults,
+    tests,
+    subjects,
+    blocks,
+  };
 }
 
 /**
@@ -115,6 +151,38 @@ async function fetchStudentTestResult(student_id) {
   }
 
   return result;
+}
+
+/**
+ * @param {Array<ObjectId>} test_ids
+ * @returns {Promise<Array>}
+ */
+
+async function loadTestsByIds(test_ids) {
+  const uniqueIds = [...new Set(test_ids.map(String))];
+  console.log("Load : test:", uniqueIds);
+  return await Loaders.test.loadMany(uniqueIds);
+}
+/**
+ * @param {Array<ObjectId>} subject_ids
+ * @returns {Promise<Array>}
+ */
+
+async function loadSubjectsByIds(subject_ids) {
+  const uniqueIds = [...new Set(subject_ids.map(String))];
+  console.log("Load : subject:", uniqueIds);
+  return await Loaders.subject.loadMany(uniqueIds);
+}
+
+/**
+ * @param {Array<ObjectId>} block_ids
+ * @returns {Promise<Array>}
+ */
+
+async function loadBlocksByIds(block_ids) {
+  const uniqueIds = [...new Set(block_ids.map(String))];
+  console.log("Load : block:", uniqueIds);
+  return await Loaders.block.loadMany(uniqueIds);
 }
 
 function TimeNow() {
