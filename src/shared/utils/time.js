10eb7e544d23ff3b7a697c4cdc01caf2aa83f3e3
zettacamp-tholib-hanafi <tholib.hanafi@zettacamp.pro>
2025-07-01 16:36:42 +0700

    [Module_3][Phase 3: Transcript Calculation][]  Update TimeNow() JS-Doc

--- src/shared/utils/time.js
+++ src/shared/utils/time.js
@@ -1,3 +1,14 @@
+/**
+ * Get the current system time formatted as "HH:MM:SS".
+ *
+ * This utility function retrieves the current time from the system clock
+ * and returns it as a string in 24-hour format, ensuring each component
+ * (hour, minute, second) is zero-padded to 2 digits.
+ *
+ * @returns {string} A string representing the current time in "HH:MM:SS" format.
+ *
+ */
+
 function TimeNow() {
   const now = new Date();
   const hours = String(now.getHours()).padStart(2, "0");
@@ -5,8 +16,8 @@ function TimeNow() {
   const seconds = String(now.getSeconds()).padStart(2, "0");
   return `${hours}:${minutes}:${seconds}`;
 }
-// *************** EXPORT MODULE ***************
 
+// *************** EXPORT MODULE ***************
 module.exports = {
   TimeNow,
 };
